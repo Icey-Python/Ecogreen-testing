f004984fdbf98b91100c7798ac998073ee4da1a4
Kelly <kelly@dev.kelly>
2024-09-19 00:15:41 +0300

    feat(admin): Added endpoints to manage products

--- controllers/admin.Controller.js
+++ controllers/admin.Controller.js
@@ -6,6 +6,7 @@ import { Logger } from 'borgen'
 import jwt from 'jsonwebtoken'
 import { Config } from '../lib/config.js'
 import Squad from '../models/squad.model.js'
+import Product from '../models/product.model.js'
 
 // @desc create new admin
 // @route POST /api/v1/admin/create
@@ -368,6 +369,117 @@ export const deleteSquadById = async (req, res) => {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       status: 'error',
       message: 'An error occurred while trying to update the squad details',
+      data: null,
+    })
+  }
+}
+
+// @desc Delete product
+// @route /api/v1/admin/delete/product/:id
+export const deleteProductById = async (req, res) => {
+  try {
+    const productId = req.params.id
+    const user = await Admin.findById(res.locals.userId)
+    // Check if the product ID is provided
+    if (!productId) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: 'error',
+        message: 'Invalid product id',
+        data: null,
+      })
+    }
+
+    if (!user || user.role != 'superAdmin') {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: 'error',
+        message: 'You are not alowed to perfom this action',
+        data: null,
+      })
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(productId)
+
+    // Send success response
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Product deleted successfully',
+      data: {
+        id: deletedProduct._id,
+        name: deletedProduct.name,
+      },
+    })
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred trying to delete the  product',
+      data: null,
+    })
+  }
+}
+
+//@ desc deactivate product
+//@ role PUT /api/v1/deactivate/product/:id
+export const deactivateProductById = async (req, res) => {
+  try {
+    let productId = req.params.id 
+
+    const updatedProduct = await Product.findByIdAndUpdate(productId, {
+      isActive: false,  
+    })
+
+    if (!updatedProduct) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        status: 'error',
+        message: 'Product not found',
+        data: null,
+      })
+    }
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Product updated successfully',
+      data: {
+        "name": updatedProduct.name
+      },
+    })
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred trying to delete the  product',
+      data: null,
+    })
+  }
+}
+
+//@ desc activate product
+//@ role PUT /api/v1/activate/product/:id
+export const activateProductById = async (req, res) => {
+  try {
+    let productId = req.params.id 
+
+    const updatedProduct = await Product.findByIdAndUpdate(productId, {
+      isActive: true,  
+    })
+
+    if (!updatedProduct) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        status: 'error',
+        message: 'Product not found',
+        data: null,
+      })
+    }
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Product updated successfully',
+      data: {
+        "name": updatedProduct.name
+      },
+    })
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred trying to delete the  product',
       data: null,
     })
   }
--- router/v1/adminGroup.js
+++ router/v1/adminGroup.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createAdmin, loginAdmin, updateAdminById, updateAdminPasswordById, deleteAdminById, getAllAdmins, getAllUsers, deleteSquadById } from "../../controllers/admin.Controller.js";
+import { createAdmin, loginAdmin, updateAdminById, updateAdminPasswordById, deleteAdminById, getAllAdmins, getAllUsers, deleteSquadById, deleteProductById, deactivateProductById, activateProductById } from "../../controllers/admin.Controller.js";
 import { adminAuth } from "../../middleware/adminAuth.js";
 
 const router = Router();
@@ -12,5 +12,8 @@ router.delete("/delete/:id",adminAuth,deleteAdminById)
 router.get("/all",adminAuth, getAllAdmins)
 router.get("/users",adminAuth, getAllUsers)
 router.delete("/delete/squad/:id",adminAuth,deleteSquadById)
+router.delete("/delete/product/:id",adminAuth,deleteProductById)
+router.put("/deactivate/product/:id",adminAuth,deactivateProductById)
+router.put("/activate/product/:id", adminAuth, activateProductById)
 
 export default router;
