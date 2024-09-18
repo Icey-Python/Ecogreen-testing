c3939d7942e1b6c03ef2c937de61746405ccc0e9
Kelly <kelly@dev.kelly>
2024-09-18 10:02:17 +0300

    feat(admin): Added get all users & admin endpoints

--- controllers/admin.Controller.js
+++ controllers/admin.Controller.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
+import User from '../models/user.model.js'
 import Admin from '../models/admin.model.js'
 import bcrypt from 'bcrypt'
 import { Logger } from 'borgen'
@@ -15,7 +16,15 @@ export const createAdmin = async (req, res) => {
       password: string
       role: string
     }**/
-
+    //check if admin is superAdmin 
+    const currentAdminUser = await Admin.findById(res.locals.userId)
+    if (currentAdminUser.role != 'superAdmin') {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: 'error',
+        message: 'You are not allowed to perfom this action',
+        data: null,
+      })
+    }
     const { name, email, password, role } = req.body
     // hash password
     const hashedPassword = bcrypt.hashSync(password, 10)
@@ -122,10 +131,10 @@ export const updateAdminById = async (req, res) => {
         data: null,
       })
     }
-    if(admin.role == "superAdmin"){
+    if (admin.role == 'superAdmin') {
       //check if the admin is superAdmin
-      const currentAdminUser = await Admin.findById(res.locals.userId);
-      if(currentAdminUser.role != "superAdmin"){
+      const currentAdminUser = await Admin.findById(res.locals.userId)
+      if (currentAdminUser.role != 'superAdmin') {
         return res.status(StatusCodes.UNAUTHORIZED).json({
           status: 'error',
           message: 'You are not allowed to perfom this action',
@@ -135,8 +144,8 @@ export const updateAdminById = async (req, res) => {
     }
 
     // Update the user
-    if(name) admin.name = name;
-    if(email) admin.email = email;
+    if (name) admin.name = name
+    if (email) admin.email = email
     const updatedAdmin = await admin.save()
 
     return res.status(StatusCodes.OK).json({
@@ -152,6 +161,152 @@ export const updateAdminById = async (req, res) => {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       status: 'error',
       message: 'An error occurred while updating the admin',
+      data: null,
+    })
+  }
+}
+
+// @desc Update admin password
+// @route PUT /api/v1/admin/password/:id
+export const updateAdminPasswordById = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const { oldPassword, newPassword } = req.body
+    // Fetch the user to update
+    const admin = await Admin.findById(userId)
+    if (!admin) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: 'error',
+        message: 'You are not allowed to perfom this action',
+        data: null,
+      })
+    }
+    if (admin.role == 'superAdmin') {
+      //check if the admin is superAdmin
+      const currentAdminUser = await Admin.findById(res.locals.userId)
+      if (currentAdminUser.role != 'superAdmin') {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: 'error',
+          message: 'You are not allowed to perfom this action',
+          data: null,
+        })
+      }
+    }
+    //compare old password
+    const isPasswordValid = await bcrypt.compare(oldPassword, admin.password)
+    if (!isPasswordValid) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: 'error',
+        message: 'Invalid old password',
+        data: null,
+      })
+    }
+    // Update the user
+    admin.password = await bcrypt.hash(newPassword, 10)
+    const updatedAdmin = await admin.save()
+
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Admin password updated successfully',
+      data: {
+        name: updatedAdmin.name,
+        email: updatedAdmin.email,
+      },
+    })
+  } catch (error) {
+    Logger.error({ message: error.message })
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred while updating the admin',
+      data: null,
+    })
+  }
+}
+
+// @desc Delete admin
+// @route DELETE /api/v1/admin/delete/:id
+export const deleteAdminById = async (req, res) => {
+  try {
+    const userId = req.params.id
+    const currentAdminUser = await Admin.findById(res.locals.userId)
+    // Fetch the user to delete
+    const admin = await Admin.findByIdAndDelete(userId)
+    if (!admin) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: 'error',
+        message: 'You are not allowed to perfom this action',
+        data: null,
+      })
+    }
+    if (admin.role == 'superAdmin' && currentAdminUser.role != 'superAdmin') {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: 'error',
+          message: 'You are not allowed to perfom this action',
+          data: null,
+        })
+    }
+
+    // superadmin delete all admin
+    if (admin.role != 'superAdmin' && userId != res.locals.userId && currentAdminUser.role != 'superAdmin') {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          status: 'error',
+          message: 'You are not allowed to perfom this action',
+          data: null,
+        })
+    }
+
+    // Delete the user
+
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Admin deleted successfully',
+      data: null,
+    })
+  } catch (error) {
+    Logger.error({ message: error.message })
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred while deleting the admin',
+      data: null,
+    })
+  }
+}
+
+// @desc Get all admins
+// @route GET /api/v1/all
+export const getAllAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.find().select('-password')
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Admins fetched successfully',
+      data: admins,
+    })
+  } catch (error) {
+    Logger.error({ message: error.message })
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred while fetching admins',
+      data: null,
+    })
+  }
+}
+
+// @desc Get all users 
+// @route GET /api/v1/admin/users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password')
+    return res.status(StatusCodes.OK).json({
+      status: 'success',
+      message: 'Users fetched successfully',
+      data: users,
+    })
+  } catch (error) {
+    Logger.error({ message: error.message })
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      message: 'An error occurred while fetching users',
       data: null,
     })
   }
--- router/v1/adminGroup.js
+++ router/v1/adminGroup.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { createAdmin, loginAdmin, updateAdminById } from "../../controllers/admin.Controller.js";
+import { createAdmin, loginAdmin, updateAdminById, updateAdminPasswordById, deleteAdminById, getAllAdmins, getAllUsers } from "../../controllers/admin.Controller.js";
 import { adminAuth } from "../../middleware/adminAuth.js";
 
 const router = Router();
 
-router.post("/create",adminAuth,createAdmin)
 router.post("/login",loginAdmin)
+router.post("/create",adminAuth,createAdmin)
 router.put("/update/:id",adminAuth,updateAdminById)
+router.put("/password/:id",adminAuth,updateAdminPasswordById)
+router.delete("/delete/:id",adminAuth,deleteAdminById)
+router.get("/all",adminAuth, getAllAdmins)
+router.get("/users",adminAuth, getAllUsers)
 
 export default router;
