cc1e04104ddff9f4dfc92cb45fdf8ee18aa9d5e4
Theeprime <areriduncan114@gmail.com>
2024-09-24 16:41:03 +0300

    feat(donations & greenbank)added donation calculation for savings in greenbank

---
+++ models/greenBank.model.js
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+
+const greenBankSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  points: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  description: {
+    type: String,
+    default: 'Extra donation points supporting environmental causes',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const GreenBank = model('GreenBank', greenBankSchema);
+export default GreenBank;
