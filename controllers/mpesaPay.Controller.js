c16294a185f36f01a9037d9d4b908e6cef04d064
Theeprime <areriduncan114@gmail.com>
2024-09-27 00:02:30 +0300

    feat(mpesa withdraw)completed mpesa payment

--- controllers/mpesaPay.Controller.js
+++ controllers/mpesaPay.Controller.js
@@ -4,22 +4,23 @@ import { Config } from "../lib/config.js";
 import { Logger } from "borgen";
 import { timestamp } from "../util/timestamp.js";
 import { StatusCodes } from "http-status-codes";
+import crypto from "crypto";
+import User from "../models/user.model.js";
 
 // Mpesa STK push
 // @route POST /api/v1/pay/stk
 export const stkPush = async (req, res) => {
   try {
     const { phone, amount } = req.body;
-    const userId = req.user.id; 
+    const userId = res.locals.userId;
 
     const mpesaToken = res.locals.token;
-
     // Fetch the user’s GreenBank and points data
-    const greenBankAccount = await GreenBank.findOne({ userId });
+    const greenBankAccount = await GreenBank.findOne({ user: userId });
     const userAccount = await User.findById(userId);
 
     if (!greenBankAccount || !userAccount) {
-      return res.status(StatusCodes.NOT_FOUND).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
         status: "error",
         message: "User or GreenBank account not found",
         data: null,
@@ -27,19 +28,18 @@ export const stkPush = async (req, res) => {
     }
 
     // Conversion logic: 100 points for every 10 KSh
-    const pointsConversionRate = 10; 
+    const pointsConversionRate = 10;
     const totalPoints = (amount * 100) / pointsConversionRate;
 
     // Calculate 10% deduction for GreenBank
-    const greenBankDeduction = amount * 0.1;
-    const netAmountToUser = amount - greenBankDeduction;
+    const greenBankDeduction = totalPoints * 0.1;
+    const netAmountToUser = totalPoints - greenBankDeduction;
 
     // Update user’s GreenBank balance
     greenBankAccount.points += greenBankDeduction;
     await greenBankAccount.save();
-
     // Update user’s points
-    userAccount.balance += totalPoints;
+    userAccount.balance += netAmountToUser;
     await userAccount.save();
 
     // Proceed with the Mpesa STK push request
@@ -63,7 +63,7 @@ export const stkPush = async (req, res) => {
       Password: password,
       Timestamp: timestamp,
       TransactionType: transactionType,
-      Amount: netAmountToUser, 
+      Amount: amount, // >10? amount : 10
       PartyA: phone,
       PartyB: BUSINESS_SHORT_CODE,
       PhoneNumber: phone,
@@ -90,7 +90,7 @@ export const stkPush = async (req, res) => {
       greenBankDeduction: greenBankDeduction,
     });
   } catch (err) {
-    Logger.error({ message: "stkPush" + err.message });
+    Logger.error({ message: "stkPush- " + err.message });
 
     res.status(HttpStatusCode.InternalServerError).json({
       status: "error",
@@ -100,7 +100,7 @@ export const stkPush = async (req, res) => {
   }
 };
 
-// Mpesa Callback
+// Mpesa Callback -> stk
 // @route POST /api/v1/mpesaPay/callback
 export const callback = async (req, res) => {
   try {
@@ -151,51 +151,71 @@ export const callback = async (req, res) => {
   }
 };
 
+const generateOriginatorConversationID = () => {
+  return crypto.randomUUID();
+};
 // @desc Withdraw from GreenBank to Mpesa
 // @route POST /api/v1/payments/withdraw
 export const withdrawToMpesa = async (req, res) => {
   try {
     const { phone, amount } = req.body;
+    if(!phone){
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: "error",
+        message: "Invalid phone number",
+        data: null,
+      });
+    }
+    if(!amount){
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        status: "error",
+        message: "Please provide a valid amount",
+        data: null,
+      });
+    }
     const userId = res.locals.userId;
     const mpesaToken = res.locals.token;
 
+    const INITIATOR_NAME = "testapi";
+    const INITIATOR_PASSWORD = "Safaricom999!*!";
+    const B2CSHORT_CODE = "600998";
+
     // Check the user's GreenBank balance
     const userGreenBank = await GreenBank.findOne({ user: userId });
-
-    if (!userGreenBank || userGreenBank.balance < amount) {
+    console.log(userGreenBank.points, "|", amount);
+    if (!userGreenBank || userGreenBank.points < amount) {
       return res.status(StatusCodes.BAD_REQUEST).json({
+        status: "error",
         message: "Insufficient balance in GreenBank.",
+        data: {
+          pts: userGreenBank.points,
+        },
       });
     }
-
+    const amountInPts = (amount * 100) / 10 ;
     // Deduct the amount from the GreenBank balance
-    userGreenBank.balance -= amount;
-    await userGreenBank.save();
+    userGreenBank.points -= amountInPts;
 
     // Mpesa transaction details
-    const CALLBACK_URL = process.env.CALLBACK_URL; 
-    const BUSINESS_SHORT_CODE = "174379"; 
-
-    const password = Buffer.from(
-      BUSINESS_SHORT_CODE + Config.LNM_PASSKEY + timestamp
-    ).toString("base64");
+    const CALLBACK_URL = process.env.CALLBACK_URL;
+    const initiatorId = generateOriginatorConversationID()
 
     const payload = {
-      BusinessShortCode: BUSINESS_SHORT_CODE,
-      Password: password,
-      Timestamp: timestamp,
-      TransactionType: "CustomerPayBillOnline", 
+      OriginatorConversationID: initiatorId,
+      InitiatorName: INITIATOR_NAME,
+      SecurityCredential:
+        "ZfBlcD1FyAv6OcVgyWjSEPBJOM8VPOC6XmahYKWwoH4NA3PrujztZQm9iQj7E9L7NsPQkOUqNI783nteUs2r1/V89MuXHBShmn5d3TqOyvj6ibAb2eMckh6s53ot73cV8I12SFg2MpnHIGNG6YgopI5WU/whZDycbbGw5U2GsTG0yBtBpz3Q6CwknN6I4RO9nS3xITfuPaixXbYYSoDWcwTSK2hs13p2/Vl5VjJUZRs7ZIm+7WKY8YiRL1u0qMmq1mSERXRAcSPwEaV8XBitchqEcrprehBzqUD7hZYD7TDbv0U/+Se2O+6khdhYYdVBAX7cCuL1nJdpcmAebWrIPA==",
+      CommandID: "BusinessPayment",
       Amount: amount,
-      PartyA: phone,
-      PartyB: BUSINESS_SHORT_CODE,
-      PhoneNumber: phone,
-      CallBackURL: `${CALLBACK_URL}/api/v1/mpesaPay/withdraw/callback`,
-      AccountReference: "GreenBank Withdrawal",
-      TransactionDesc: "Withdraw to Mpesa",
+      PartyA: B2CSHORT_CODE,
+      PartyB: phone,
+      Remarks: "Withdrawal from Greenbank",
+      QueueTimeOutURL: `${CALLBACK_URL}/api/v1/mpesaPay/b2c/timeout`,
+      ResultURL: `${CALLBACK_URL}/api/v1/mpesaPay/b2c/result`,
     };
 
     const response = await axios.post(
-      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      "https://sandbox.safaricom.co.ke/mpesa/b2c/v3/paymentrequest",
       payload,
       {
         headers: {
@@ -209,6 +229,8 @@ export const withdrawToMpesa = async (req, res) => {
       message: "Withdrawal in progress",
       data: response.data,
     });
+
+    await userGreenBank.save();
   } catch (err) {
     Logger.error("Withdrawal Error:", err.message);
 
@@ -220,54 +242,47 @@ export const withdrawToMpesa = async (req, res) => {
 };
 
 /// Mpesa Withdrawal Callback
-// @route POST /api/v1/mpesaPay/withdraw/callback
-export const mpesaWithdrawalCallback = async (req, res) => {
+// @route POST /api/v1/mpesaPay/b2c/result
+export const b2cResultCallback = async (req, res) => {
   try {
-    const { Body } = req.body;
-    const { stkCallback } = Body;
+    const { Result } = req.body;
 
-    const resultCode = stkCallback.ResultCode;
-    const resultDesc = stkCallback.ResultDesc;
-    const callbackMetadata = stkCallback.CallbackMetadata;
+    const {
+      ResultType,
+      ResultCode,
+      ResultDesc,
+      OriginatorConversationID,
+      ConversationID,
+      TransactionID,
+      ReferenceData,
+    } = Result;
 
-    if (resultCode === 0) {
-      const amount = callbackMetadata.Item.find(
-        (item) => item.Name === "Amount"
-      ).Value;
-      const mpesaReceiptNumber = callbackMetadata.Item.find(
-        (item) => item.Name === "MpesaReceiptNumber"
-      ).Value;
-      const transactionDate = callbackMetadata.Item.find(
-        (item) => item.Name === "TransactionDate"
-      ).Value;
-      const phoneNumber = callbackMetadata.Item.find(
-        (item) => item.Name === "PhoneNumber"
-      ).Value;
+    return res.status(StatusCodes.OK).json({
+      message: "Withdrawal processed successfully",
+      data: {},
+    });
+  } catch (err) {
+    console.log(err);
+    console.log(stkCallBack);
+    Logger.error("Withdrawal Callback Error:", err.message);
 
-      const transaction_date = new Date(transactionDate);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "An error occurred during Mpesa callback",
+      error: err.message,
+    });
+  }
+};
 
-      console.log("Mpesa Withdrawal Successful:", {
-        mpesaReceiptNumber,
-        amount,
-        transaction_date,
-        phoneNumber,
-      });
+// @ desc Withdrawal timeout
+// @ route POST /api/v1/mpesaPay/b2c/timeout
+export const b2cTimeoutCallback = async (req, res) => {
+  try {
+    console.log(req.body);
 
-      return res.status(StatusCodes.OK).json({
-        message: "Withdrawal processed successfully",
-        data: {
-          amount,
-          mpesaReceiptNumber,
-          transaction_date,
-          phoneNumber,
-        },
-      });
-    } else {
-      Logger.error(`Withdrawal Failed: ${resultDesc}`);
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: `Withdrawal failed: ${resultDesc}`,
-      });
-    }
+    return res.status(StatusCodes.OK).json({
+      message: "Timeout occurred perfoming this action",
+      data: {},
+    });
   } catch (err) {
     Logger.error("Withdrawal Callback Error:", err.message);
 
--- router/v1/mpesaPayGroup.js
+++ router/v1/mpesaPayGroup.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { generateToken } from "../../middleware/genToken.js";
-import { callback, stkPush ,withdrawToMpesa,mpesaWithdrawalCallback} from "../../controllers/mpesaPay.Controller.js";
-
+import {
+  callback,
+  stkPush,
+  withdrawToMpesa,
+  b2cTimeoutCallback,
+  b2cResultCallback,
+} from "../../controllers/mpesaPay.Controller.js";
+import { userAuth } from "../../middleware/userAuth.js";
 const router = Router();
 
-router.post("/stk", generateToken, stkPush);
+router.post("/stk", userAuth, generateToken, stkPush);
 router.post("/callback", callback);
-router.post("/withdraw", withdrawToMpesa);
-router.post("/withdraw/callback", mpesaWithdrawalCallback);
-
+router.post("/withdraw", userAuth, generateToken, withdrawToMpesa);
+router.post("/b2c/result", b2cResultCallback);
+router.post("/b2c/timeout", b2cTimeoutCallback);
 export default router;
