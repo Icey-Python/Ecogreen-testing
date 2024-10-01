162101bd6fef51d216c4797129160e1c77210691
Kelly <kelly@dev.kelly>
2024-10-01 17:54:09 +0300

    feat(order): Add share order to order group

--- router/v1/cartGroup.js
+++ router/v1/cartGroup.js
@@ -1,15 +1,21 @@
-import { Router } from "express";
-import { userAuth } from "../../middleware/userAuth.js";
-import { addItemToCart, checkout, deleteCartItem, getCartItems, updateCartItem } from "../../controllers/cart.Controller.js";
+import { Router } from 'express'
+import { userAuth } from '../../middleware/userAuth.js'
+import {
+  addItemToCart,
+  checkout,
+  deleteCartItem,
+  getCartItems,
+  getSharedOrder,
+  updateCartItem,
+} from '../../controllers/cart.Controller.js'
 
+const router = Router()
 
+router.post('/add', userAuth, addItemToCart)
+router.put('/update', userAuth, updateCartItem)
+router.get('/all', userAuth, getCartItems)
+router.delete('/delete/:productId', userAuth, deleteCartItem)
+router.post('/checkout', userAuth, checkout)
+router.get('/shared/:orderId', userAuth, getSharedOrder)
 
-const router = Router();
-
-router.post("/add",userAuth, addItemToCart );
-router.put("/update",userAuth, updateCartItem );
-router.get("/all",userAuth, getCartItems)
-router.delete("/delete/:productId",userAuth, deleteCartItem );
-router.post("/checkout",userAuth, checkout);
-
-export default router;
+export default router
