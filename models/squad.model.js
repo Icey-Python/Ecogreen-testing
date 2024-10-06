b0a5dd50538a50885ba079b8b91ef38a84e24aab
Kelly <kelly@dev.kelly>
2024-10-06 10:23:16 +0300

    fix(squad): Fix squads requested Members

--- models/squad.model.js
+++ models/squad.model.js
@@ -7,48 +7,47 @@ const squadSchema = new Schema(
       required: true,
     },
     members: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User', // Each squad has members who are users
-      }],
-    requestedMembers: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    moderators: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Moderator",
-      },
-    ],
-    posts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Post",
-      },
-    ],
-    description: {
-        type: String,
-      },
-    admin:{
-        type: Schema.Types.ObjectId,
-        ref:'User'
-    },
-    percentageAchieved:{
-      type: Number,
-      default: 0,
-      min: 0,
-      max: 100
-    },
-    carbonCalculatorData:{
       type: Schema.Types.ObjectId,
-      ref: 'CarbonCalculator',
+      ref: 'User', // Each squad has members who are users
+    }],
+    requestedMembers: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+        default: [],
+    },
+moderators: [
+  {
+    type: Schema.Types.ObjectId,
+    ref: "Moderator",
+  },
+],
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+    description: {
+  type: String,
+      },
+admin: {
+  type: Schema.Types.ObjectId,
+    ref: 'User'
+},
+percentageAchieved: {
+  type: Number,
+      default: 0,
+    min: 0,
+      max: 100
+},
+carbonCalculatorData: {
+  type: Schema.Types.ObjectId,
+    ref: 'CarbonCalculator',
     }
   },
-  { timestamps: true }
+{ timestamps: true }
 );
 
-const  Squad = model("Squad", squadSchema);
+const Squad = model("Squad", squadSchema);
 
 export default Squad;
